3141d0a291cfd96ca3e4f3a0fd75a941cc76134a
Majri Abdelali <abdelali.majri@solutiondata.ma>
2018-05-28 11:39:25 +0000

    mise en place des fixtures

--- dbd.sql
+++ dbd.sql
@@ -5,3 +5,6 @@ INSERT INTO `ticket_status` (`id`, `label`, `color`, `code`) VALUES
   (4, 'Intervention', '.', '4'),
   (5, 'Remise en service', '.', '5'),
   (6, 'Clôture', '.', '6');
+
+
+UPDATE `user` SET `password` = '$2y$13$lbt1s.A6tNJ.7a2WQLi24O0WayN4QzrTsvlyvQK0xFd2Gzpid4jyy' WHERE `user`.`login` = 'amajri';
