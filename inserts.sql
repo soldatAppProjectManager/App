8bc7455633284419ad552401cbf00f5990a02eb7
Majri Abdelali <abdelali.majri@solutiondata.ma>
2018-06-11 16:10:54 +0000

    add medele CNOPS

---
+++ inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO `rfp_modele` (`id`, `label`) VALUES ('3', 'CNOPS');
+INSERT INTO `modele_devis` (`id`, `nom`, `description`, `modele`, `langue`, `miseenpage`)
+VALUES (NULL, 'C.N.O.P.S', 'Bordereau des prix CNOPS', 'cnops.html.twig', 'Français', 'portrait');
